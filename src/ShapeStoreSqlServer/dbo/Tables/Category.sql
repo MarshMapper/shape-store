c9a0408f77a6759e6900ef945a834d73e32d44c6
Paul <paul@goodhops.com>
2024-08-26 10:03:20 -0400

    Add database project for the SQL Server objects needed by the service

---
+++ src/ShapeStoreSqlServer/dbo/Tables/Category.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [CategoryId]  INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50)  NOT NULL,
+    [IconId]      INT            NULL,
+    [Description] NVARCHAR (256) NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([CategoryId] ASC),
+    CONSTRAINT [FK_Category_Icon] FOREIGN KEY ([IconId]) REFERENCES [dbo].[Icon] ([IconId])
+);
+
---
+++ src/ShapeStoreSqlServer/dbo/Tables/Location.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Location] (
+    [LocationId]  INT               IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (256)    NULL,
+    [Address1]    NVARCHAR (256)    NULL,
+    [Address2]    NVARCHAR (256)    NULL,
+    [City]        NVARCHAR (96)     NULL,
+    [State]       NVARCHAR (50)     NULL,
+    [PostalCode]  NVARCHAR (16)     NULL,
+    [Phone]       NVARCHAR (20)     NULL,
+    [Phone2]      NVARCHAR (50)     NULL,
+    [Url]         NVARCHAR (512)    NULL,
+    [Coordinates] [sys].[geography] NULL,
+    [IsOpen]      BIT               NULL,
+    [CategoryId]  INT               NULL,
+    [IconId]      INT               NULL,
+    [Notes]       NVARCHAR (1024)   NULL,
+    CONSTRAINT [PK_Location] PRIMARY KEY CLUSTERED ([LocationId] ASC),
+    CONSTRAINT [FK_Location_Category] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([CategoryId])
+);
+
